d4471c0cdda92321a7409f33c1f75608ce84d2dc
Gigi-23 <gigi.espo1997@gmail.com>
2025-04-28 16:17:01 +0200

    Add files via upload

---
+++ CORSO JAVA 2/28-04-2025/Prova variabili.java
@@ -0,0 +1,41 @@
+public class Prova variabili {
+    public static void main(String[] args) {
+        System.out.printl(3);
+        System.out.printl(358);
+        System.out.printl(50000);
+        // System.out.printl("Hello World"); //
+        /*System.out.println(Ciao MONDO"); */
+
+    int. provaNumero = 12;
+    String provaTesto = "Ciao Mondo";
+    boolean provaBool = true;
+    
+    final int provaNumero = 15;
+    provaNumero = 20;
+
+String testo = "Prova"
+System.out.printl("Ciao " + testo);
+
+String firstPart = "Ciao"
+
+int primoNumero= 5;
+int secondoNumero = 6;
+System.out.println (primoNumero + secondoNumero);
+
+int numeroUno = 5, numeroDue = 6, numeroTre= 50;
+System.out.println(numeroUno + numeroDue + numeroTre);
+
+int numeroUno, numeroDue, numeroTre;
+numeroUno = numeroDue = numeroTre = 50;
+System.out.println(numeroUno + numeroDue + numeroTre);
+
+float myFloat= 5.75f;
+double myDouble = 19.99d;
+float myFloat = 35e3f;
+double myDouble = 12E4d;
+
+boolean isJava = true;
+boolean isFerrari= false;
+
+    }
+}
